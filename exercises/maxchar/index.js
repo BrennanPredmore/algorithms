65019db20ba8554e908f841b5d01a736fc6d9b70
BrennnanPredmore <brennnann@gmail.com>
2021-02-16 02:28:22 -0600

    notes init for revesrseINT // solution for maxChar and creating an object with for of loop

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+    // THIS "FOR OF" LOOP, LOOPS THROUGH A STRING AND CREATES AN OBJECT OF THE CHARMAP
+    for (let char of str){
+        if(charMap[char]){
+            charMap[char]++
+        }else {
+            charMap[char] = 1
+        }
+    }
+   
+    // THIS "FOR IN" LOOP, LOOPS THROUGH N OBJECT
+    for (let char in charMap){
+        if(charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,12 +9,25 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  const reversed = n.toString().split('').reverse().join('');
+  // ASSIGN VARIABLE REVERSED TO THE PLACEHOLDER
+  const reversed = n
+  // TURN THE PLACEHOLDER TO A STRING FROM AN INT
+  .toString()
+  // SPLIT THE STRING IN ARRAY
+  .split('')
+  // REVERSE THE CHARACTERS OF THE ARRAY
+  .reverse()
+  // JOIN THEM BACK TO TOGETHER BUT NOT IN REVERSE
+  .join('');
 
+  // THIS HANDLES FOR NEGATIVE INSTANCES
   if (n < 0) {
+    // PARSEINT TURNS THE STRING INTO AN INT
+    // WE TAKE ANY CASE LESS THAT ZERO 
+    // AND MULTIPLE IT BY -1 TO AUTOMATICALLY CHANGE IT TO A NEGATIVE
     return parseInt(reversed) * -1;
   }
-
+// ALL OTHER CASES (MORE THAN ZERO) WILL RETURN A PARSEINT POSITIVE REVERSED NUMBER!
   return parseInt(reversed);
 }
 
